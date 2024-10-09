927a4f1da3272d0b1659983851ea431059d632b5
Gudu300 <gudusuja321@gmail.com>
2024-10-09 23:03:36 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7845280686:AAFlKaW28B1blZyMuW6iDIVEJaru6OLhO_Y")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Suryakant")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("2047564483")
+
+
+
+
+
+
+
+  
